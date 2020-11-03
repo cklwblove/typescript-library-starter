5326a944e1832916f057be6b43319708478cf42a
cklwblove <lwbhtml@163.coom>
2020-11-03 16:13:10 +0800

    refactor: 💡 升级rollup版本及相关插件

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,30 +1,73 @@
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
 import sourceMaps from 'rollup-plugin-sourcemaps'
+import { terser } from 'rollup-plugin-terser'
 import camelCase from 'lodash.camelcase'
 import typescript from 'rollup-plugin-typescript2'
-import json from 'rollup-plugin-json'
+import json from '@rollup/plugin-json'
 
 const pkg = require('./package.json')
 
+const external = [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})]
+
 const libraryName = '--libraryname--'
 
-export default {
+/**
+ * Comment with library information to be appended in the generated bundles.
+ */
+const banner = `/*!
+ * ${pkg.name} v${pkg.version}
+ * (c) ${pkg.author}
+ * Released under the ${pkg.license} License.
+ */
+`
+
+/**
+ * Creates an output options object for Rollup.js.
+ * @param {import('rollup').OutputOptions} options
+ * @returns {import('rollup').OutputOptions}
+ */
+function createOutputOptions(options) {
+  return {
+    banner,
+    name: `${libraryName}`,
+    sourcemap: true,
+    ...options,
+  };
+}
+
+/**
+ * @type {import('rollup').RollupOptions}
+ */
+const options = {
   input: `src/${libraryName}.ts`,
   output: [
-    { file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true },
-    { file: pkg.module, format: 'es', sourcemap: true },
+    createOutputOptions({
+      file: pkg.module,
+      format: 'esm',
+    }),
+    createOutputOptions({
+      file: pkg.main,
+      format: 'umd',
+      name: `${camelCase(libraryName)}`
+    }),
+    createOutputOptions({
+      file: `./dist/${libraryName}.umd.min.js`,
+      format: 'umd',
+      plugins: [terser()],
+      name: `${camelCase(libraryName)}`
+    }),
   ],
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
-  external: [],
+  external,
   watch: {
-    include: 'src/**',
+    include: 'src/**'
   },
   plugins: [
     // Allow json resolution
     json(),
     // Compile TypeScript files
-    typescript({ useTsconfigDeclarationDir: true }),
+    typescript(),
     // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
     commonjs(),
     // Allow node_modules resolution, so you can use 'external' to control
@@ -33,6 +76,8 @@ export default {
     resolve(),
 
     // Resolve source maps to the original source
-    sourceMaps(),
+    sourceMaps()
   ],
-}
+};
+
+export default options;
